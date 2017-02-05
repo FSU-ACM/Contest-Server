ba99afea93acd558865e39c49904e9055b18fe94
andrewsosa001 <andrewsosa001@gmail.com>
2017-02-05 16:37:21 -0500

    Adds dynamic config.

--- README.md
+++ README.md
@@ -3,3 +3,21 @@ Flask suite for running Fall/Spring Programming Contests
 
 Here is the requirements documents:
 https://docs.google.com/document/d/1_9VTQSOmZ_X8lVzaUFmRy_2ldRHsIqK-1720amgIq0U/edit?usp=sharing
+
+
+## Configuration
+Configuration is handled by creating a `/instance` folder in the project's
+root directory. Inside, create a file `config.py` in which to declare
+the required variables. Below is a sample configuration.
+
+```
+# General dev config
+DEBUG = True
+
+# SQLAlchemy Configuration
+SQLALCHEMY_DATABASE_URI = 'mysql://root:acmAtFSU@localhost/spcWebDB'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+```
+
+These config values override the basic config values from the root's
+`/config.py` file
--- app/__init__.py
+++ app/__init__.py
@@ -4,17 +4,17 @@ from flask_bootstrap import Bootstrap
 from flask_nav import Nav
 from flask_nav.elements import *
 
-# Init
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
+# Init & Config
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
 
-# Config
+# Init modules
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
+# Other Config
 app.url_map.strict_slashes = False
 
-# DB Config
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:acmAtFSU@localhost/spcWebDB'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://outUser:spc17ftw@localhost/spcWebDB'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(app)
 
 from app import views
