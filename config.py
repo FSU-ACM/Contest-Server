4ac695911e2bac5fd93a422561676a871ba95a43
andrewsosa001 <andrewsosa001@gmail.com>
2017-02-07 22:30:11 -0500

    Removes secret key from version control.
    Except not really because it's still visible in the history. Will need to remove via tool soon.

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ SQLALCHEMY_DATABASE_URI = None              # Must be overridden with /instance
 # ReCaptcha
 RECAPTCHA_ENABLED = True
 RECAPTCHA_SITE_KEY = '6LeyxBQUAAAAAHBpG2htNplW1qjcgODp47P6FHuE'
-RECAPTCHA_SECRET_KEY = '6LeyxBQUAAAAABa9gQkkSRmpKEjeWej-aEpHtomD'
+RECAPTCHA_SECRET_KEY = ''
