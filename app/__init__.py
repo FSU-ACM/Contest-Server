f2d0cf6aa8c18592dc103978a2e47598b19d43f9
andrewsosa001 <andrewsosa001@gmail.com>
2017-02-09 17:29:10 -0500

    Adds cache-busting

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,8 @@ from flask_recaptcha import ReCaptcha
 from flask_nav import Nav
 from flask_nav.elements import *
 
+import os
+
 # Init & Config
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
@@ -17,6 +19,22 @@ recaptcha = ReCaptcha(app=app)
 
 # Other Config
 app.url_map.strict_slashes = False
+
+
+# Cache busting
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
 
 
 from app import views
