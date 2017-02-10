89b8534bab69e877933485643b9b648c3f97814c
andrewsosa001 <andrewsosa001@gmail.com>
2017-02-10 15:11:52 -0500

    Adds Sponsors to index

--- debug.py
+++ debug.py
@@ -2,4 +2,5 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(threaded=True,host='0.0.0.0', port=8000, debug=True)
+    # app.run(threaded=True,host='0.0.0.0', port=8000, debug=True)
+	app.run(port=8000)
