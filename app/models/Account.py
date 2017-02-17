dabde7d32ce25343ab9d4baa52a1c566c09c1d87
Juan Francisco Alfaro <franciscoalfaro2008@gmail.com>
2017-02-17 17:05:58 -0500

    Finishes the Profile model and Account model
    Adds the following attributes for Profile:
            gradyear,gradterm,advProg,status, account_id 
    Fixes Account.password: 
           from Date to String

--- app/models/Account.py
+++ app/models/Account.py
@@ -3,7 +3,7 @@ from app import db
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=False, nullable=False)
-    password = db.Column(db.Date, unique=False, nullable=False)
+    password = db.Column(db.String(128), unique=False, nullable=False)
 
     # category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
     # category = db.relationship('Category',
--- app/models/Profile.py
+++ app/models/Profile.py
@@ -2,28 +2,42 @@ from app import db
 
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    f_name = db.Column(db.String(128), unique=False, nullable=False)
+    l_name = db.Column(db.String(128), unique=False, nullable=False)
     fsuid = db.Column(db.String(64), unique=True)
-    name = db.Column(db.String(128), unique=False, nullable=False)
     dob = db.Column(db.Date, unique=False, nullable=False)
-    gender = db.Column(db.String(64), unique=True)
-    race = db.Column(db.String(64), unique=True)
-    major = db.Column(db.String(64), unique=True)
-    year = db.Column(db.String(64), unique=True)
-    mostProg = db.Column(db.String(64), unique=True)
+    gender = db.Column(db.String(64), unique=False)
+    race = db.Column(db.String(64), unique=False)
+    foodallergies = db.Column(db.String(512), unique=False)
+    major = db.Column(db.String(64), unique=False)
+    year = db.Column(db.String(64), unique=False)
+    gradyear = db.Column(db.Integer, unique=False)
+    gradterm = db.Column(db.String(64), unique=False)
+    advProg = db.Column(db.String(64), unique=False)
+    status = db.Column(db.String(64), unique=False)
 
     # Account relationship
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'))
     account = db.relationship("Account", back_populates="profile")
 
-    def __init__(self, fsuid, name,email=None, gender=None):
+    def __init__(self,f_name,l_name,fsuid=None,\
+                 dob,gender=None,race=None,foodallergies=None,\
+                 major=None,year=None,gradyear=None,gradterm=None,\
+                 advProg=None,status=None,account_id):
+        self.f_name = f_name
+        self.l_name = l_name
         self.fsuid = fsuid
-        self.name = name
         self.dob = dob
         self.gender = gender
         self.race = race
+        self.foodallergies = foodallergies
         self.major = major
         self.year = year
-        self.mostProg = mostProg
+        self.gradyear = gradyear
+        self.gradterm = gradterm
+        self.advProg = advProg
+        self.status = status
+        self.account_id = account_id 
 
     def __repr__(self):
         return '<Student %r>' % self.fsuid
