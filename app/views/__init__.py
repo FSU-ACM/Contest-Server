5bb591d46d0214acce62c66f410db948c9a73b00
Andrew Sosa <andrewsosa001@gmail.com>
2018-03-10 13:41:34 -0500

    Update some templates

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -12,12 +12,12 @@ def index():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('common/404.html'), 404
 
 
 @app.errorhandler(500)
 def page_error(e):
-    return render_template('500.html'), 500
+    return render_template('common/500.html'), 500
 
 
 # @app.route('/allteams')
