0b137ce81e0c7a9de8fce09e926a86c63239002d
Andrew Sosa <andrewsosa001@gmail.com>
2018-05-07 16:29:18 -0400

    Change CSV route to class based view and apply is_admin

--- app/views/admin/user_csv.py
+++ app/views/admin/user_csv.py
@@ -3,50 +3,59 @@
 from io import StringIO
 import csv
 
-from flask import make_response
+from flask import make_response, redirect, url_for
+from flask.views import View
 
-from app import app, basic_auth
 from app.models import Account
+from app.util import session as session_util
 
 
-@app.route('/admin/users/csv')
-@basic_auth.required
-def user_csv():
-    """
+class UserCsvView(View):
+    """Download a CSV with all user information (not passwords).
+
+    Only accessible for is_admin accounts.
+
     Downloads a CSV containing user data elements:
-        - Email
-        - First Name
-        - Last Name
-        - FSUID
-        - Team Name
-        - Team ID
+    - Email
+    - First Name
+    - Last Name
+    - FSUID
+    - Team Name
+    - Team ID
     """
-    users = [(
-        account.id,
-        account.first_name,
-        account.last_name,
-        account.fsuid,
-        ((account.team and account.team.team_name) or ''),
-        ((account.team and account.team.teamID) or ''),
-        (account.signin is not None)
-    ) for account in Account.objects.all()]
 
-    # Add CSV header
-    users.insert(0, (
-        'Email',
-        'First Name',
-        'Last Name',
-        'FSUID',
-        'Team Name',
-        'Team ID',
-        'Checked In'
-    ))
+    def dispatch_request(self):
+        account = session_util.get_account()
+        if not account.is_admin():
+            return redirect(url_for('login'))
+        else:
 
-    si = StringIO()
-    cw = csv.writer(si)
-    cw.writerows(users)
+            users = [(
+                account.id,
+                account.first_name,
+                account.last_name,
+                account.fsuid,
+                ((account.team and account.team.team_name) or ''),
+                ((account.team and account.team.teamID) or ''),
+                (account.signin is not None)
+            ) for account in Account.objects.all()]
 
-    res = make_response(si.getvalue())
-    res.headers['Content-Disposition'] = 'attachment; filename=mycsv.csv'
-    res.mimetype = 'text/csv'
-    return res
+            # Add CSV header
+            users.insert(0, (
+                'Email',
+                'First Name',
+                'Last Name',
+                'FSUID',
+                'Team Name',
+                'Team ID',
+                'Checked In'
+            ))
+
+            si = StringIO()
+            cw = csv.writer(si)
+            cw.writerows(users)
+
+            res = make_response(si.getvalue())
+            res.headers['Content-Disposition'] = 'attachment; filename=mycsv.csv'
+            res.mimetype = 'text/csv'
+            return res
