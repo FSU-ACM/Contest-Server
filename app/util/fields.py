60470c1bc248d46895d7387bc0995983655c6eb3
Andrew Sosa <andrewsosa001@gmail.com>
2018-07-20 10:50:57 -0400

    Add FSUIDField for FSUID normailzation

--- app/util/fields.py
+++ app/util/fields.py
@@ -1,4 +1,5 @@
 from wtforms.fields.html5 import EmailField as wtf_EmailField
+from wtforms import StringField as wtf_StringField
 
 class EmailField(wtf_EmailField):
     """Email field for WTForms
@@ -14,6 +15,23 @@ class EmailField(wtf_EmailField):
 
     def process_formdata(self, valuelist):
         super(wtf_EmailField, self).process_formdata(valuelist)
+
+        if valuelist and len(valuelist) >= 1:
+            self.data = valuelist[0].lower()
+
+
+class FSUIDField(wtf_StringField):
+    """FSUID field for custom WTForms.
+
+    This field will normalize FSUIDs to be all lowercase.
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        wtf_StringField.__init(self, *args, **kwargs)
+
+    def process_formdata(self, valuelist):
+        super(wtf_StringField, self).process_formdata(valuelist)
 
         if valuelist and len(valuelist) >= 1:
             self.data = valuelist[0].lower()
