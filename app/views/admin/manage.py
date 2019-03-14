d5fa670ffd38956d338946842178205b27100be9
manterolat <manterolat@users.noreply.github.com>
2019-03-13 21:20:29 -0400

    Use SelectField for division in Course admin panel

--- app/views/admin/manage.py
+++ app/views/admin/manage.py
@@ -2,6 +2,7 @@ from flask import flash, redirect, url_for, request, Markup
 
 from flask_admin import AdminIndexView
 from flask_admin.contrib.mongoengine import ModelView
+from flask_admin.form.fields import Select2Field
 
 from app.models.Account import Account
 from app.util import session as session_util
@@ -95,6 +96,26 @@ class CourseManageView(BaseManageView):
     column_searchable_list = ('name', 'professor_name', 'professor_email',)
     column_editable_list = ('name', 'professor_name', 'division')
     column_display_pk = True
+
+    def scaffold_form(self):
+        """Replaces Division field with a Select field in the edit form.
+
+        """
+        
+        form = super().scaffold_form()
+        form.division = Select2Field('Division', coerce=int, choices=[(1, "Upper"), (2, "Lower")])
+        return form
+
+
+    def scaffold_list_form(self, widget=None, validators=None):
+        """Replaces Division field with a Select field in the editable column.
+        
+        """
+
+        form_class = super().scaffold_list_form(widget=widget, validators=validators)
+        form_class.division = Select2Field('Division', coerce=int, choices=[(1, "Upper"), (2, "Lower")])
+        return form_class
+
 
     def on_model_delete(self, model):
         """Removes all references to deleted course to avoid dangling
